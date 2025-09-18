f127b72522a42f91b069953393aebc30e365a0d3
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-18 16:25:11 +0200

    Craete CardNew NewsDetail news.ts rewrite Home and routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
+import news from "../data/news.json";
+import CardNew from "../components/CardNew";
+import type { NewsItem } from "../types/news";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen p-8">
-      <h1 className="text-4xl font-bold">Home</h1>
-      <p className="mt-3 text-slate-600">
-        Вітаю! Це головна сторінка. Tailwind працює, якщо бачиш відступи та
-        стилі.
-      </p>
-    </main>
-  );
+    const items = news as NewsItem[];
+
+    return (
+        <main className="mx-auto max-w-4xl px-4 py-8">
+            <header className="mb-6 flex items-center justify-between">
+                <h1 className="text-2xl font-bold">News</h1>
+                <Link to="/" className="text-sm underline">Main</Link>
+            </header>
+
+            <section className="grid gap-4 sm:grid-cols-1">
+                {items.map((n) => (
+                    <CardNew key={n.id} item={n} />
+                ))}
+            </section>
+        </main>
+    );
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,16 +3,20 @@ import Layout from "../components/Layout";
 import Articles from "../pages/Articles";
 import Home from "../pages/Home";
 import Sport from "../pages/Sport";
+import NewsDetails from "../pages/NewsDetails";
+import { Navigate } from "react-router-dom";
+
 
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <Layout />, // <- постійний Header тут
+    element: <Layout />, //
     children: [
-      { index: true, element: <Home /> }, // те саме, що path: ''
+      { index: true, element: <Home /> },
       { path: "articles", element: <Articles /> },
       { path: "sport", element: <Sport /> },
-      { path: "*", element: <Home /> }, // fallback (можеш замінити на NotFound)
+      { path: "news/:id", element: <NewsDetails /> },
+      { path: "*", element: <Navigate to="/" replace /> },
     ],
   },
 ];

845f2ab1339b53056b6fe49af0c9f6e676045566
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-19 14:41:06 +0200

    Lttle check

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,6 @@
-import {lazy} from "react";
-import type {RouteObject} from "react-router-dom";
-import {Navigate} from "react-router-dom";
+import { lazy } from "react";
+import type { RouteObject } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 // import ProtectedRoute from "./ProtectedRoute";
 import PublicOnlyRoute from "./PublicOnlyRoute";
 
@@ -16,35 +16,35 @@ const SignUp = lazy(() => import("../pages/SignUp"));
 const Sport = lazy(() => import("../pages/Sport"));
 
 const routes: RouteObject[] = [
-    {
-        path: "/",
-        element: <Layout/>,
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "articles", element: <Articles /> },
+      { path: "sport", element: <Sport /> },
+      { path: "about", element: <About /> },
+      { path: "news/:id", element: <NewsDetails /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <SignUp /> },
+
+      //middlware
+      // {
+      //   element: <ProtectedRoute />,
+      //   children: [{ path: "bookmarks", element: <Bookmarks /> }],
+      // },
+
+      {
+        element: <PublicOnlyRoute />,
         children: [
-            {index: true, element: <Home/>},
-            {path: "articles", element: <Articles/>},
-            {path: "sport", element: <Sport/>},
-            {path: "about", element: <About/>},
-            {path: "news/:id", element: <NewsDetails/>},
-            {path: "login", element: <Login/>},
-            {path: "signup", element: <SignUp/>},
-
-            //middlware
-            // {
-            //   element: <ProtectedRoute />,
-            //   children: [{ path: "bookmarks", element: <Bookmarks /> }],
-            // },
-
-            {
-                element: <PublicOnlyRoute/>,
-                children: [
-                    {path: "login", element: <Login/>},
-                    {path: "signup", element: <SignUp/>},
-                ],
-            },
-
-            {path: "*", element: <Navigate to="/" replace/>},
+          { path: "login", element: <Login /> },
+          { path: "signup", element: <SignUp /> },
         ],
-    },
+      },
+
+      { path: "*", element: <Navigate to="/" replace /> },
+    ],
+  },
 ];
 
 export default routes;
